3381b4120f148ce42617cbbbcd5e01aa47231e23
Aditya Lakshmi Narayana Karedla <88328060+adityakaredla@users.noreply.github.com>
2023-11-23 18:58:51 +0530

    Update inclusionSearch.js

--- inclusionSearch.js
+++ inclusionSearch.js
@@ -1,10 +1,10 @@
 
 function inclusionSearch(dataObj, input) {
     for (const key in dataObj) {
-        if (typeof dataObj[key] === 'string' && dataObj[key].toLowerCase().includes(input.toLowerCase())) {
+        if (typeof dataObj[key] === 'string' && dataObj[key].toLowerCase().includes(input.trim().toLowerCase())) {
             return true;
         }
-        if (typeof dataObj[key] === 'number' && dataObj[key].toString().toLowerCase().includes(input.toLowerCase())) {
+        if (typeof dataObj[key] === 'number' && dataObj[key].toString().toLowerCase().includes(input.trim().toLowerCase())) {
             return true;
         }
         if (typeof dataObj[key] === 'object' && inclusionSearch(dataObj[key], input)) {
